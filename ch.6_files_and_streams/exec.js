1e0d850fe60b6c9b2b1c53326611b6b38627ba00
Carlos Zabaleta <zabaletacopa@gmail.com>
2019-09-05 00:42:37 -0400

    Create child process

---
+++ ch.6_files_and_streams/exec.js
@@ -0,0 +1,14 @@
+const cp = require('child_process')
+
+// cp.exec("open https://www.lynda.com/")
+
+// cp.exec("open -a Terminal .")
+
+// cp.exec("ls", (err, data) => {
+//   if(err) throw err
+//   console.log(data)
+// })
+
+cp.exec("node readStream", (err, data, stderr) => {
+  console.log(data);
+});
